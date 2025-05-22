bffcb5d497359ce6af1cc751c8e7d60980cffabc
Ronnin-01 <omarmd425@gmail.com>
2025-05-22 17:15:31 +0530

    SideBar Ui Done

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:perplexity_clone/widgets/side_bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          //side navbar
+          SideBar(),
+          Column(
+            children: [
+              //search section
+              //footer
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
